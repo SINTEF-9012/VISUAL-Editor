8c5161ebcc548a969bfe51c98951804145e4cc23
Antoine Pultier <antoine.pultier@sintef.no>
2014-12-18 14:17:41 +0100

    I tried Dijkstra and Douglas-Peucker algorithms

--- app/scripts/main3.js
+++ app/scripts/main3.js
@@ -5,9 +5,8 @@ var height = window.innerHeight,
 
 var first = true;
 
-
 window.onload = function() {
-	$('svg path').remove();
+	$('svg path, svg rect').remove();
 	var boxes = $('.drag');
 	var connectedBoxes = [];
 
@@ -19,7 +18,7 @@ window.onload = function() {
 
 		if (first){
 			(new Draggabilly(this,{grid: [25,25]})).on('dragEnd', function(){
-				$('svg path').remove();
+				$('svg path, svg rect').remove();
 				drawGrid(boxes);
 				connectBoxes(connectedBoxes);
 			});
@@ -52,6 +51,12 @@ var connect = function(bA, bB) {
 	var posA = bA.position(),
 		posB = bB.position();
 
+	if (posA.left > posB.left) {
+		var posC = posB;
+		posB = posA;
+		posA =  posC;
+	}
+
 
 	var startA = {
 		x: posA.top + bA.width()/2,
@@ -72,10 +77,21 @@ var connect = function(bA, bB) {
 		.attr("d", lineFunction([startA, startB]))
 		.attr("stroke-width", 3)
 		.attr("stroke", "brown");
+
+	var newLine = [startA, {
+		y: posA.left + (bA.width() * (startA.y > startB.y ? 2 : 2)),
+		x: startA.x
+	}, {
+		y: posB.left - 100,
+		x: startB.x,
+	}, startB] 
+	var line = svg.append("path")
+		.attr("d", lineFunction(newLine))
+		.attr("stroke-width", 3)
+		.attr("stroke", "orange");
 }
 
 var drawGrid = function(boxes){
-	return;
 	var vertical = [0, window.innerWidth];
 	var horizontal = [0, window.innerHeight];
 
@@ -85,6 +101,8 @@ var drawGrid = function(boxes){
 		.x(function(d){return d.y;})
 		.y(function(d){return d.x;})
 		.interpolate("linear");
+
+	var computedBoxes = [];
 
 	boxes.each(function(){
 		var jthis = $(this);
@@ -96,11 +114,46 @@ var drawGrid = function(boxes){
 		horizontal.push(pos.top+height);
 		vertical.push(pos.left);
 		vertical.push(pos.left+width);
+
+		computedBoxes.push({
+			startX: pos.left,
+			startY: pos.top,
+			endX: pos.left+width,
+			endY: pos.top+height,
+			centerX: pos.left+width/2,
+			centerY: pos.top+height/2
+		});
 	});
 
-	vertical.sort();
+	var sortFunction = function(a,b) {
+		return a - b;
+	}
 
-	console.log(vertical);
+	var newVertical = [], last = null;
+	vertical.sort(sortFunction).forEach(function(t) {
+		if (t !== last) {
+			for (var i = last + 50; i < t; i += 50) {
+				newVertical.push(i);
+			}
+			newVertical.push(t);
+			last = t;
+		}
+	});
+	vertical = newVertical;
+
+	var newHorizontal = [];
+	last = null;
+	horizontal.sort(sortFunction).forEach(function(t) {
+		if (t !== last) {
+			for (var i = last + 50; i < t; i += 50) {
+				newHorizontal.push(i);
+			}
+			newHorizontal.push(t);
+			last = t;
+		}
+	});
+
+	horizontal = newHorizontal;
 
 	vertical.forEach(function(t){
 		var line = svg.append("path")
@@ -112,6 +165,141 @@ var drawGrid = function(boxes){
 		var line = svg.append("path")
 			.attr("d", lineFunction([
 				{y: 0, x: t}, {y: window.innerWidth, x:t}]))
-			.attr("stroke", "orange");
+			.attr("stroke", "aqua");
 	});
+
+	console.log(vertical, horizontal);
+	var grid = [], astarGrid = [];
+
+	for (var i = 1, l = horizontal.length; i < l; ++i) {
+		var row = [], astarRow = [];
+
+		for (var ii = 1, ll = vertical.length; ii < ll; ++ii) {
+
+			var startY = horizontal[i-1],
+				startX = vertical[ii-1],
+				endY = horizontal[i],
+				endX = vertical[ii],
+				centerX = (startX+endX)/2,
+				centerY = (startY+endY)/2,
+				score = 0;
+
+			computedBoxes.forEach(function(boxe){
+				// console.log(boxe.startX == startX, boxe.startY == startY,
+						// boxe.endX == endX, boxe.endY == endY);
+				// if (boxe.startX === startX)
+				// console.log(boxe.startX, startX, boxe.endX, endX)
+				// console.log(boxe.startY, startY, boxe.endY, endY)
+				if (startX >= boxe.startX && startY >= boxe.startY &&
+					endX <= boxe.endX && endY <= boxe.endY) {
+					score += 5;
+				} else {
+					var distance = (centerX - boxe.centerX) * (centerX - boxe.centerX)
+						+ (centerY - boxe.centerY) * (centerY - boxe.centerY);
+
+					var c = 1/(distance/5000);
+					if (c > 0.05)
+						score += c*1.33;
+				}
+			});
+
+			svg.append("rect")
+				.attr("fill", score > 1 ? "red" : "blue")
+				.attr("opacity", Math.min(0.3, score/5))
+				.attr("x", startX)
+				.attr("y", startY)
+				.attr("width", endX-startX)
+				.attr("height", endY-startY);
+
+			row.push({
+				startX: startX,
+				startY: startY,
+				endX: endX,
+				endY: endY,
+				score: score,
+				centerX: (startX + endX)/2,
+				centerY: (startY + endY)/2
+			});
+
+			astarRow.push((score+1));
+		}
+		grid.push(row);
+		astarGrid.push(astarRow);
+	}
+
+
+	console.log(grid);
+
+	var astarGraph = new Graph(astarGrid);
+	var getCost = function(old) {
+		if (!old.t) old.t = 1;
+		if (old.x === this.x) {
+			// console.log("VERTICAL");
+			this.d = 1;
+		} else if (old.y === this.y) {
+			// console.log("HORIZONTAL");
+			this.d = 2;
+		}
+		if (old.d !== this.d) {
+			this.t = old.t + 1;
+		} else {
+			this.t = old.t;
+		}
+		return this.type * 10 + this.t;
+	}
+	astarGraph.nodes.forEach(function(n){
+		n.getCost = getCost;
+	});
+	var search = astar.search(
+		astarGraph,
+		astarGraph.grid[0][0],
+		astarGraph.grid[grid.length-3][grid[0].length-3],
+		{
+			heuristic: function(a,b){
+				return 0;
+				var cellA = grid[a.x][a.y],
+					cellB = grid[b.x][b.y];
+				// 	console.log(cellA, cellB)
+				var d1 = Math.abs(cellB.centerX - cellA.centerX);
+				var d2 = Math.abs(cellB.centerY - cellA.centerY);
+				// // console.log(b.x, a.x);
+				// if (b.x != a.x || b.y != a.y) {
+				// 	d1 += 100;
+				// }
+				return d1 + d2;
+			}
+		});
+	console.log(search);
+
+	var lineFunction = d3.svg.line()
+		.x(function(d){return d.x;})
+		.y(function(d){return d.y;})
+		.interpolate("linear");
+
+	var lineData = [];
+
+	search.unshift(astarGraph.grid[0][0]);
+	search.forEach(function(t) {
+		var cell = grid[t.x][t.y];
+		svg.append("rect")
+			.attr("fill", "green")
+			.attr("opacity", 0.5)
+			.attr("x", cell.startX)
+			.attr("y", cell.startY)
+			.attr("width", cell.endX-cell.startX)
+			.attr("height", cell.endY-cell.startY);
+		lineData.push({
+			x: cell.centerX,
+			y: cell.centerY
+		})
+
+	});
+
+	console.log(lineData.length)
+	lineData = simplify(lineData, 40);
+	console.log(lineData.length)
+	svg.append("path")
+		.attr("d", lineFunction(lineData))
+		.attr("stroke-width", 3)
+		.attr("stroke", "green");
 }
